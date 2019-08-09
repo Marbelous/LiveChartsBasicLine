d996725727018b647f948afefd7eeb5b790108eb
zendruid <zendruid@hotmail.com>
2019-08-09 15:48:16 -0400

    Add project files.

---
+++ LiveChartsBasicLine/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace Wpf.CartesianChart.PointShapeLine
+{
+    public partial class PointShapeLineExample : UserControl
+    {
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+        public PointShapeLineExample()
+        {
+            InitializeComponent();
+
+            InitChart();
+
+            DataContext = this;
+        }
+
+        private void InitChart()
+        {
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 1, 2, 3, 4 ,5 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 5, 4, 3 ,2, 1 },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 1, 3, 5, 3, 1 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 6, 4, 2, 4, 6 },
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+                PointGeometrySize = 50,
+                PointForeground = Brushes.Gray
+            });
+
+            //modifying any series values will also animate and update the chart
+            //SeriesCollection[ 3 ].Values.Add(5d);
+        }
+
+        private void QuitBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void ResetBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            InitChart();
+        }
+    }
+}
